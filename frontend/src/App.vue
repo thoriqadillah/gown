911e86a200623086430adcd64cf8e4f44aee3ed0
thoriqadillah <thoriqadillah59@gmail.com>
2023-04-07 07:46:34 +0700

    add tailwind css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import HelloWorld from './components/HelloWorld.vue'
 import { useTheme } from './stores/theme'
 import { Theme } from '../wailsjs/go/main/App'
 
@@ -11,5 +10,23 @@ Theme().then(theme => {
 </script>
 
 <template>
-  <HelloWorld/>
+  <div class="flex w-full justify-center h-screen items-center">
+    <p>Center</p>
+  </div>
 </template>
+
+<style>
+body {
+  font-family: "Roboto", -apple-system, BlinkMacSystemFont, "Segoe UI",
+  "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+  sans-serif;
+}
+
+@font-face {
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  src: local(""),
+  url("assets/fonts/roboto-regular.woff2") format("woff2");
+}
+</style>
