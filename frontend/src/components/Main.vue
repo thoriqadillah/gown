62927a9871a5ff68e3617b7fc8b62b4917759ead
thoriqadillah <thoriqadillah59@gmail.com>
2023-04-09 09:49:49 +0700

    change navigation layout

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -8,13 +8,13 @@ const store = useDrawerStore()
 <template>
   <v-card>
     <v-layout >
-      <v-navigation-drawer v-model="store.drawer" elevation="2" expand-on-hover rail :border="0">
+      <v-navigation-drawer v-model="store.drawer" :elevation="0" expand-on-hover rail :border="0" location="right">
         <v-list density="compact" nav class="tw-flex tw-flex-col tw-mt-20">
-          <v-list-item prepend-icon="mdi-home" title="Home" value="home"></v-list-item>
-          <v-list-item prepend-icon="mdi-alert-outline" title="Failed Download" value="failedDownload"></v-list-item>
-          <v-list-item prepend-icon="mdi-tray-full" title="Queued Download" value="queuedDownload"></v-list-item>
-          <v-list-item prepend-icon="mdi-cog-outline" title="Settings" value="settings"></v-list-item>
-          <v-list-item prepend-icon="mdi-information-outline" title="About" value="about"></v-list-item>
+          <v-list-item active-color="primary" prepend-icon="mdi-home" title="Home" value="home"></v-list-item>
+          <v-list-item active-color="primary" prepend-icon="mdi-alert-outline" title="Failed Download" value="failedDownload"></v-list-item>
+          <v-list-item active-color="primary" prepend-icon="mdi-tray-full" title="Queued Download" value="queuedDownload"></v-list-item>
+          <v-list-item active-color="primary" prepend-icon="mdi-cog-outline" title="Settings" value="settings"></v-list-item>
+          <v-list-item active-color="primary" prepend-icon="mdi-information-outline" title="About" value="about"></v-list-item>
         </v-list>
       </v-navigation-drawer>
 
--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -35,52 +35,58 @@ const store = useDrawerStore()
 </script>
 
 <template>
-  <nav class="tw-mx-5 tw-my-5 tw-flex tw-justify-between tw-gap-10 tw-items-center">
-    <div class="tw-flex tw-gap-5 tw-items-center">
-      <v-tooltip v-if="ratio.width.value < RAIL_MIN_WIDTH_RATIO" text="Menu" location="bottom">
-        <template v-slot:activator="{ props }">
-          <v-btn v-bind="props" density="compact" icon="mdi-dots-vertical" variant="flat" @click="store.openDrawer()"/>
-        </template>
-      </v-tooltip>
+  <nav class="tw-mx-5 tw-my-5 tw-flex tw-justify-between tw-gap-8 tw-items-center">
+    <p class="text-button">GOWN</p>
 
-      <p class="text-button">GOWN</p>
+    <v-text-field :loading="loading" density="compact" variant="outlined" color="primary" label="Search" append-inner-icon="mdi-magnify" single-line hide-details @click:append-inner="search" v-on:keyup.enter="search" ref="input"/>
 
+    <div class="tw-flex tw-gap-5">
       <v-tooltip :text="themeTooltip" location="bottom">
         <template v-slot:activator="{ props }">
           <v-btn v-bind="props" density="compact" :icon="themeIcon" @click="toggleTheme" variant="flat"/>
         </template>
       </v-tooltip>
-    </div>
-    <v-text-field :loading="loading" density="compact" variant="outlined" color="primary" label="Search" append-inner-icon="mdi-magnify" single-line hide-details @click:append-inner="search" v-on:keyup.enter="search" ref="input"/>
-    <div class="tw-flex tw-gap-5">
+
+      <v-divider vertical/>
+
       <v-tooltip text="Resume All" location="bottom">
         <template v-slot:activator="{ props }">
           <v-btn v-bind="props" density="compact" icon="mdi-play" variant="flat"/>
         </template>
       </v-tooltip>
+
       <v-tooltip text="Pause All" location="bottom">
         <template v-slot:activator="{ props }">
           <v-btn v-bind="props" density="compact" icon="mdi-pause" variant="flat"/>
         </template>
       </v-tooltip>
+
       <v-tooltip text="Stop All" location="bottom">
         <template v-slot:activator="{ props }">
           <v-btn v-bind="props" density="compact" icon="mdi-stop" variant="flat"/>
         </template>
       </v-tooltip>
-      <v-divider vertical></v-divider>
+
+      <v-divider vertical/>
       
       <v-tooltip text="Add into Queue" location="bottom">
         <template v-slot:activator="{ props }">
           <v-btn v-bind="props" density="compact" icon="mdi-tray-plus" variant="flat"/>
         </template>
       </v-tooltip>
+
       <v-tooltip text="New" location="bottom">
         <template v-slot:activator="{ props }">
           <v-btn v-bind:="props" variant="flat" density="compact" icon>
             <v-icon icon="mdi-plus"></v-icon>
             <new-download/>
           </v-btn>
+        </template>
+      </v-tooltip>
+
+      <v-tooltip text="Menu" location="bottom">
+        <template v-slot:activator="{ props }">
+          <v-btn class="xl:tw-hidden" v-bind="props" density="compact" icon="mdi-dots-vertical" variant="flat" @click="store.openDrawer()"/>
         </template>
       </v-tooltip>
     </div>
