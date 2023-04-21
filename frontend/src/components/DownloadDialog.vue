d075e2de8eac33d5ffd9dcfefae69097c6a69865
thoriqadillah <thoriqadillah59@gmail.com>
2023-04-21 10:03:38 +0700

    add validation

--- frontend/src/components/DownloadDialog.vue
+++ frontend/src/components/DownloadDialog.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { download } from '../../wailsjs/go/models';
 import Dialog from '../services/download-dialog'
 import Downloader from '../services/downloader'
@@ -16,11 +16,41 @@ const activator = ref(false)
 const loaded = ref(false)
 const onFile = ref(false)
 const onURL = ref(true)
-const url = ref('')
 const size = ref('')
+const url = ref('')
+const urlErr = ref('')
+const urlHasError = ref(false)
+const savelocation = ref('')
+const savelocationErr = ref('')
+const savelocationHasError = ref(false)
+const filename = ref('')
+const filenameErr = ref('')
+const filenameHasError = ref(false)
 
 const dialog = new Dialog({ activator, loaded, loading, onFile, onURL })
 const downloader = new Downloader()
+
+watch(url, (newval, oldval) => {
+  urlHasError.value = url.value.trim().length === 0
+  urlErr.value = url.value.trim().length === 0 ? 'This field is required' : ''
+})
+watch(filename, (newval, oldval) => {
+  if (filename.value.trim().length === 0) {
+    filenameErr.value = 'This field is required'
+    filenameHasError.value = true
+  } else if (filename.value.trim().length > 256) {
+    filenameErr.value = 'File name must be below 256 characters'
+    filenameHasError.value = true
+  } else {
+    filenameErr.value = ''
+    filenameHasError.value = false
+  }
+})
+watch(savelocation, (newval, oldval) => {
+  savelocationHasError.value = savelocation.value.trim().length === 0
+  savelocationErr.value = savelocation.value.trim().length === 0 ? 'This field is required' : ''
+})
+
 
 async function fetch() {
   try {
@@ -29,12 +59,16 @@ async function fetch() {
     
     result.value = await downloader.fetch(url.value) as download.Download
     size.value = downloads.parseSize(result.value.size)
+    filename.value = result.value.name
+    savelocation.value = settings.value.saveLocation
     
     dialog.done()
     dialog.next()
   } catch (error) {
     input.value.blur()
     dialog.done()
+    urlHasError.value = true
+    urlErr.value = error as string
   }
 }
 
@@ -50,11 +84,11 @@ async function execute() {
 <template>
   <v-dialog v-model="activator" activator="parent" max-width="450px" transition="dialog-top-transition" persistent>
     <v-card>
-      <v-text-field v-if="onURL || !loaded" v-model="url" :loading="loading" :autofocus="activator" color="primary" type="input" hint="Click enter to fetch the file data from the URL you want to download" class="tw-p-3" density="compact" variant="outlined" label="URL" append-inner-icon="mdi-link" append-icon="mdi-magnify" @click:append="fetch()" single-line v-on:keyup.enter="fetch()" ref="input"/>
+      <v-text-field v-if="onURL || !loaded" :error="urlHasError" :error-messages="urlErr" v-model="url" :loading="loading" :autofocus="activator" color="primary" type="input" hint="Click enter to fetch the file data from the URL you want to download" class="tw-p-3" density="compact" variant="outlined" label="URL" append-inner-icon="mdi-link" append-icon="mdi-magnify" @click:append="fetch()" single-line v-on:keyup.enter="fetch()" ref="input"/>
       <div v-else-if="onFile || !loaded" class="tw-flex tw-items-center">
         <div class="tw-basis-9/12">
-          <v-text-field color="primary" v-model="result.name" label="File name" append-inner-icon="mdi-file-document-edit" class="tw-px-3 tw-pt-3 -tw-mb-4" single-line v-on:keyup.enter="fetch" density="compact" variant="outlined" ref="input"/>
-          <v-text-field color="primary" v-model="settings.value.saveLocation" label="Save location" append-inner-icon="mdi-folder" type="input" hint="Save location" class="tw-p-3" single-line v-on:keyup.enter="fetch" density="compact" variant="outlined" ref="input"/>
+          <v-text-field color="primary" :error="filenameHasError" :error-messages="filenameErr" v-model="filename" label="File name" append-inner-icon="mdi-file-document-edit" type="input" hint="File name" class="tw-px-3 tw-pt-3 -tw-mb-2" single-line density="compact" variant="outlined" />
+          <v-text-field color="primary" :error="savelocationHasError" :error-messages="savelocationErr" v-model="savelocation" label="Save location" append-inner-icon="mdi-folder" type="input" hint="Save location" class="tw-p-3" single-line density="compact" variant="outlined" />
         </div>
         <div class="tw-basis-3/12 tw-text-center tw-pr-2 -tw-mt-5">
           <v-icon icon="mdi-file"></v-icon>
@@ -73,7 +107,7 @@ async function execute() {
         </div>
         <div class="tw-flex tw-flex-row-reverse">
           <v-card-actions>
-            <v-btn color="primary" block @click="execute()" :disabled="onURL || !loaded">Download</v-btn>
+            <v-btn color="primary" block @click="execute()" :disabled="(onURL || !loaded) || urlHasError || filenameHasError || savelocationHasError">Download</v-btn>
           </v-card-actions>
           <v-card-actions>
             <v-btn variant="text" block @click="dialog.close()">Cancel</v-btn>
