73999cf8a574284027f595d3ac33363033d31ade
thoriqadillah <thoriqadillah59@gmail.com>
2023-04-27 22:53:09 +0700

    simplifying

--- frontend/src/components/DownloadDialog.vue
+++ frontend/src/components/DownloadDialog.vue
@@ -10,7 +10,7 @@ import Dialog from '../services/download-dialog'
 const downloads = useDownloads()
 const settings = useSettings()
 
-const result = ref(new download.Download())
+const result = ref<download.Download>()
 const input = ref()
 const loading = ref(false)
 const activator = ref(false)
@@ -19,34 +19,24 @@ const onFile = ref(false)
 const onURL = ref(true)
 
 let url = ref('')
-const urlErr = ref('')
+
 const urlHasError = ref(false)
-const savelocationErr = ref('')
 const savelocationHasError = ref(false)
-const filenameErr = ref('')
 const filenameHasError = ref(false)
+
+const validate = ref({
+  required: (v: string) => !!v.trim() || "This field cannot be empty",
+  max: (v: string) => v.trim().length < 256 || "File name must be below 256 characters"
+})
+
+watch([url, () => result.value?.name, settings], () => {
+  urlHasError.value = url.value.trim().length === 0
+  savelocationHasError.value = settings.saveLocation.trim().length === 0
+  filenameHasError.value = result.value!.name.trim().length === 0 || result.value!.name.trim().length > 256
+})
 
 const dialog = new Dialog({ activator, loaded, loading, onFile, onURL })
 const downloader = new Downloader()
-
-watch([url, () => result.value.name, settings], () => {
-  urlHasError.value = url.value.trim().length === 0
-  urlErr.value = url.value.trim().length === 0 ? 'This field is required' : ''
-  
-  savelocationHasError.value = settings.saveLocation.trim().length === 0
-  savelocationErr.value = settings.saveLocation.trim().length === 0 ? 'This field is required' : ''
-
-  if (result.value.name.trim().length === 0) {
-    filenameErr.value = 'This field is required'
-    filenameHasError.value = true
-  } else if (result.value.name.trim().length > 256) {
-    filenameErr.value = 'File name must be below 256 characters'
-    filenameHasError.value = true
-  } else {
-    filenameErr.value = ''
-    filenameHasError.value = false
-  }
-})
 
 async function fetch() {
   try {
@@ -60,37 +50,29 @@ async function fetch() {
   } catch (error) {
     input.value.blur()
     dialog.done()
-    urlHasError.value = true
-    urlErr.value = error as string
+    // TODO: add alert to error message
   }
 }
 
 async function execute() {
   dialog.close()
-  downloader.download(result.value)
-  downloads.add(result.value)
-  url = ref('')
-  urlErr.value = ''
-  urlHasError.value = false
-  filenameErr.value = ''
-  filenameHasError.value = false
-  savelocationErr.value = ''
-  savelocationHasError.value = false
+  downloader.download(result.value!)
+  downloads.add(result.value!)
 }
 </script>
 
 <template>
   <v-dialog v-model="activator" activator="parent" max-width="450px" transition="dialog-top-transition" persistent>
     <v-card>
-      <v-text-field v-if="onURL || !loaded" :error="urlHasError" :error-messages="urlErr" v-model="url" :loading="loading" :autofocus="activator" color="primary" type="input" hint="Click enter to fetch the file data from the URL you want to download" class="tw-p-3" density="compact" variant="outlined" label="URL" append-inner-icon="mdi-link" append-icon="mdi-magnify" @click:append="fetch()" single-line v-on:keyup.enter="fetch()" ref="input"/>
+      <v-text-field v-if="onURL || !loaded" :rules="[validate.required]" v-model="url" :loading="loading" :autofocus="activator" color="primary" type="input" hint="Click enter to fetch the file data from the URL you want to download" class="tw-p-3" density="compact" variant="outlined" label="URL" append-inner-icon="mdi-link" append-icon="mdi-magnify" @click:append="fetch()" single-line v-on:keyup.enter="fetch()" ref="input"/>
       <div v-else-if="onFile || !loaded" class="tw-flex tw-items-center">
         <div class="tw-basis-9/12">
-          <v-text-field color="primary" :error="filenameHasError" :error-messages="filenameErr" v-model="result.name" label="File name" append-inner-icon="mdi-file-document-edit" type="input" hint="File name" class="tw-px-3 tw-pt-3 -tw-mb-2" single-line density="compact" variant="outlined" />
-          <v-text-field color="primary" :error="savelocationHasError" :error-messages="savelocationErr" v-model="settings.saveLocation" label="Save location" append-inner-icon="mdi-folder" type="input" hint="Save location" class="tw-p-3" single-line density="compact" variant="outlined" />
+          <v-text-field color="primary" :rules="[validate.required, validate.max]" v-model="result!.name" label="File name" append-inner-icon="mdi-file-document-edit" type="input" hint="File name" class="tw-px-3 tw-pt-3 -tw-mb-2" single-line density="compact" variant="outlined" />
+          <v-text-field color="primary" :rules="[validate.required]" v-model="settings.saveLocation" label="Save location" append-inner-icon="mdi-folder" type="input" hint="Save location" class="tw-p-3" single-line density="compact" variant="outlined" />
         </div>
         <div class="tw-basis-3/12 tw-text-center tw-pr-2 -tw-mt-5">
-          <v-icon :icon="result.type.icon" :color="result.type.color"></v-icon>
-          <p class="text-body-1 tw-mt-5">{{ parseSize(result.size) }}</p>
+          <v-icon :icon="result!.type.icon" :color="result!.type.color"></v-icon>
+          <p class="text-body-1 tw-mt-5">{{ parseSize(result!.size) }}</p>
         </div>
       </div>
       
